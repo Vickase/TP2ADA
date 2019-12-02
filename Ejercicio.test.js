f3c4f87198726e2b9e34a2d51ca127de07969834
Victoria Aseff <vickase.a@gmail.com>
2019-12-02 11:02:43 -0300

    Identado

--- Ejercicio.test.js
+++ Ejercicio.test.js
@@ -1,58 +1,62 @@
-
 const ventas = require("./Ejercicio").ventas;
 const vendedoras = require("./Ejercicio").vendedoras;
 const precios = require("./Ejercicio").precios;
-const sucursales = require('./Ejercicio').sucursales;
+const sucursales = require("./Ejercicio").sucursales;
 const precioMaquina = require("./Ejercicio").precioMaquina;
-const ventasSucursal=require("./Ejercicio").ventasSucursal;
-const ventasVendedora=require("./Ejercicio").ventasVendedora;
-const mejorVendedora=require("./Ejercicio").mejorVendedora;
-const ventaPromedio= require("./Ejercicio").ventaPromedio;
-const obtenerIdVenta=require("./Ejercicio").obtenerIdVenta;
+const ventasSucursal = require("./Ejercicio").ventasSucursal;
+const ventasVendedora = require("./Ejercicio").ventasVendedora;
+const mejorVendedora = require("./Ejercicio").mejorVendedora;
+const ventaPromedio = require("./Ejercicio").ventaPromedio;
+const obtenerIdVenta = require("./Ejercicio").obtenerIdVenta;
 const agregarVenta = require("./Ejercicio").agregarVenta;
-
 
 test("Recibe un array de componentes y devuelve la suma de los precios", () => {
   const componentes = ["Monitor GPRS 3000", "Motherboard ASUS 1500"];
   const resultado = precioMaquina(componentes);
   expect(resultado).toBe(320);
 });
-test ("Recibe un nombre de una vendedora y devuelve el total de ventas de esta",()=>{
-    const nombre= "Fio";
-    const resultado3=ventasVendedora(nombre);
-    expect(resultado3).toBe(830);
-    });
-
-test ("Recibe una sucursal y devuelve el total de ventas de esta",()=>{
-const sucursal= "Centro";
-const resultado2=ventasSucursal(sucursal);
-expect(resultado2).toBe(990)
+test("Recibe un nombre de una vendedora y devuelve el total de ventas de esta", () => {
+  const nombre = "Fio";
+  const resultado3 = ventasVendedora(nombre);
+  expect(resultado3).toBe(830);
 });
 
-test ("Devuelve el nombre de la vendedora que más ingresos tuvo",()=>{
-const rdo1=ventasVendedora("Fio");
-const rdo2=ventasVendedora("Vick");
-const rdo3=ventasVendedora("Hermenegilda");
-const rdo4=ventasVendedora("Julia");
-const funcion=mejorVendedora();
-expect(funcion).toBe("Fio");
-
+test("Recibe una sucursal y devuelve el total de ventas de esta", () => {
+  const sucursal = "Centro";
+  const resultado2 = ventasSucursal(sucursal);
+  expect(resultado2).toBe(990);
 });
 
-test("devuelve un importe promedio por venta", () =>{
-    expect(ventaPromedio()).toBe(353);
+test("Devuelve el nombre de la vendedora que más ingresos tuvo", () => {
+  const rdo1 = ventasVendedora("Fio");
+  const rdo2 = ventasVendedora("Vick");
+  const rdo3 = ventasVendedora("Hermenegilda");
+  const rdo4 = ventasVendedora("Julia");
+  const funcion = mejorVendedora();
+  expect(funcion).toBe("Fio");
+});
+
+test("devuelve un importe promedio por venta", () => {
+  expect(ventaPromedio()).toBe(353);
 });
 
 test("devuelve un número random entre 100000000 y 999999999", () => {
-    let venta = Math.round(Math.random() * (999999999 +1) - 100000000) + 100000000;
-    expect(obtenerIdVenta(venta)).toBeGreaterThan(99999999)
-    expect(obtenerIdVenta(venta)).toBeLessThan(1000000000)
+  let venta =
+    Math.round(Math.random() * (999999999 + 1) - 100000000) + 100000000;
+  expect(obtenerIdVenta(venta)).toBeGreaterThan(99999999);
+  expect(obtenerIdVenta(venta)).toBeLessThan(1000000000);
 });
 
-test("agrega una nueva venta al array de ventas", () =>{
-    let id = obtenerIdVenta();
-    ventas.push([id, dia= 4, mes= 6, año= 1871, nombreVendedora= "Hermenegilda", sucursal= "Flores", componente= "sdgasaj"]);
-    expect(ventas.length).toBe(7);
+test("agrega una nueva venta al array de ventas", () => {
+  let id = obtenerIdVenta();
+  ventas.push([
+    id,
+    (dia = 4),
+    (mes = 6),
+    (año = 1871),
+    (nombreVendedora = "Hermenegilda"),
+    (sucursal = "Flores"),
+    (componente = "sdgasaj")
+  ]);
+  expect(ventas.length).toBe(7);
 });
-
-
